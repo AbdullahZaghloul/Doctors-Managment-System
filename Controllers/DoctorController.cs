55dbb690b25a276c1710f5475320169165003970
Abdullah Zaghloul <abdullahzaghloul022@gmail.com>
2025-03-28 23:06:08 +0200

    first commit

---
+++ Controllers/DoctorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Task1.Data;
+
+namespace MVC_Task1.Controllers
+{
+    public class DoctorController : Controller
+    {
+        private readonly ApplicationDbContext _context = new();
+        public IActionResult BookAppointment()
+        {
+            var doctors = _context.Doctors;
+            return View(doctors.ToList());
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Task1.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Doctors;Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
